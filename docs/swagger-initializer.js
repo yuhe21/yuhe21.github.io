d0410a8e65296b9b40cee191c82450e4c1c730b7
yuhe0221 <yamasaki.yuhei@revamp.co.jp>
2025-03-19 18:29:40 +0900

    dini

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,11 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [ 
+      "./openAPI/openapi.yml",
+      "./openAPI/openapi2.yml",
+      "./openAPI/openapi3.yml"
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
