cf81f4d7f282754aa4acf5a76e9ebe4b254b317f
yuhe0221 <yamasaki.yuhei@revamp.co.jp>
2025-03-19 18:51:45 +0900

    dini

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,7 +1,14 @@
-window.onload = function() {
+window.onload = async function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  try {
+    const response = await fetch("./urls/index.json");
+    if (!response.ok) throw new Error("JSON fetch error");
+    const files = await response.json()?.files;
+  } catch (error) {
+    console.error("Error loading Swagger files:", error);
+}
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
